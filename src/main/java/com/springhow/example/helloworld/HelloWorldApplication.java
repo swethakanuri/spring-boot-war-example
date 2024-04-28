db870ed983fd772553e8fea5570a645690fbe84a
sharafat678 <124073003+sharafat678@users.noreply.github.com>
2024-04-28 18:08:43 +0500

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello-World2";
+        return "Hello-World3";
     }
 
 }
