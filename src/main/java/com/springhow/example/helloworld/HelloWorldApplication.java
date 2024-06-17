eb578eab1458a5e0905ad524eb8ff08861527a5c
Sourav Mondal <souravmondal10@gmail.com>
2024-06-17 21:29:41 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello-World3";
+        return "Hello-World34";
     }
 
 }
