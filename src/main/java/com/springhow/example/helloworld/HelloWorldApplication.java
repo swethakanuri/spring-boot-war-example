ce2b3d12d2e2e1ab82b699f777a67bba07634166
Kanuri Swetha <kanuri.10741827@ltimindtree.com>
2024-06-18 13:31:52 +0530

    this file is modified

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello-World34";
+        return "Hello-World34567";
     }
 
 }
