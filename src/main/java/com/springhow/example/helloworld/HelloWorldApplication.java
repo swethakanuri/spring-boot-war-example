6d4b51f4745c5193b96f0f6fa9154bf86483fe90
swethakanuri <61013408+swethakanuri@users.noreply.github.com>
2024-06-22 15:47:58 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello-World1234--swetha";
+        return "Hello-World1234--swetha-kanuri";
     }
 
 }
